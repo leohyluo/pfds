07ce3eb1aec7d1c5d6ca9f1cac6fcd4fb64adbf6
leohyluo <32305819@qq.com>
2018-03-18 23:12:13 +0800

    add hystrix dashboard

--- pfds-user/src/main/java/com/iebm/pfds/user/UserStarter.java
+++ pfds-user/src/main/java/com/iebm/pfds/user/UserStarter.java
@@ -1,20 +1,17 @@
 package com.iebm.pfds.user;
 
+import com.iebm.pfds.commons.core.annotation.SpringCloudApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.ComponentScan;
-
-import com.iebm.pfds.commons.core.annotation.SpringCloudApplication;
 
 @SpringCloudApplication
 @ComponentScan(basePackages = {"com.iebm.pfds.**"})
 @EntityScan(basePackages = {"com.iebm.pfds.commons.pojo"})
 @ServletComponentScan(basePackages={"com.iebm.pfds.commons.config"})
 @EnableFeignClients(basePackages = {"com.iebm.pfds.service.rpc.user"})
-@EnableHystrixDashboard
 public class UserStarter {
     public static void main(String[] args) {
         SpringApplication.run(UserStarter.class, args);
